ce4940bd23181d598eb948388100d18eb585ebe1
유영진 <contentsware@gmail.com>
2020-11-04 21:01:47 +0900

    pick

---
+++ RandomPosition/src/pick/Pick.java
@@ -0,0 +1,55 @@
+package pick;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Pick {
+
+	public static void main(String[] args) {
+
+		// 배열 생성 : new String[18]
+		// 배열 : 인스턴스다
+		String[] members = { "고한경", "김정현", "김효영", "남서아", "서지민", "양선경", "이도경", "이수진", "이정민", "장명지", "장윤혁", "정인수", "진효선",
+				"최정은", "최지혜", "최혜인", "한우리", "한주량" };
+
+		ArrayList<String> memberList = new ArrayList<String>();
+
+		for (int i = 0; i < members.length; i++) {
+			memberList.add(members[i]);
+		}
+
+		Collections.shuffle(memberList);
+
+		for (String name : memberList) {
+			System.out.print(name+" ");
+		}
+		System.out.println();
+
+		String[][] position = new String[6][];
+		position[0] = new String[2];
+		position[1] = new String[4];
+		position[2] = new String[3];
+		position[3] = new String[3];
+		position[4] = new String[3];
+		position[5] = new String[3];
+
+		System.out.println("=============================================");
+
+		int index = 0;
+
+		for (int i = 0; i < position.length; i++) {
+			for (int j = 0; j < position[i].length; j++) {
+				position[i][j] = memberList.get(index++);
+			}
+		}
+		
+		for (String[] pos : position) {
+			for (String name : pos) {
+				System.out.print(name + "\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
