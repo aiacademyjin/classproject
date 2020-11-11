af7d16baaa86697ceefa6dd709f1b7f730dd8ce8
aiacademyjin <aia.jin.202009@gmail.com>
2020-11-11 15:12:49 +0900

    Update 2020.11.11.sql

--- SQL/2020.11.11.sql
+++ SQL/2020.11.11.sql
@@ -124,4 +124,126 @@ where c.custid=o.custid(+)
 group by c.name
 ;
 
+--===============================================================
+
+-- 서브 쿼리
+-- 스칼라 부속질의 – SELECT 부속질의, 단일 행 단일열
+-- 인라인 뷰      – FROM 부속질의, 복합 행 복합 열
+-- 중첩질의       – WHERE 부속질의, 단일 열, 단순비교 -> 단일 행, 집합 비교 -> 복합 행
+
+-- 중찹 질의 : 단순 비교
+
+-- 평균 급여(sub query)보다 더 많은 급여를 받는 사원을 출력
+
+select avg(sal) from emp;
+--2073.214285714285714285714285714285714286
+
+select *
+from emp
+where sal > 2073.214285714285714285714285714285714286-- 평균 급여
+;
+
+select *
+from emp
+where sal > (select avg(sal) from emp)-- 평균 급여
+;
+
+-- sal 300 이상 받는 사원이 소속된 부서
+select distinct deptno from emp where sal >= 3000;
+
+select *
+from emp
+--where deptno=10 or deptno=20
+where deptno in (select distinct deptno from emp where sal >= 3000)
+;
+
+
+-- SCOTT 사원의 같은 부서에 근무하는 사원의 리스트를 추력
+select deptno from emp where ename='SCOTT';
+
+select ename , deptno
+from emp
+where deptno=(select deptno from emp where ename='SCOTT') -- SCOTT이 소속된 부서 번호
+;
+
+-- 30번 소속 사원들 중에서 급여를 가장 많이 받는 사원보다 
+-- 더 많은 급여를 받는 
+-- 사람의 이름, 급여를 출력하는 쿼리문을 작성해 봅시다.
+
+select max(sal) from emp where deptno=30;
+select sal from emp where deptno=30;
+
+select ename, sal
+from emp
+--where sal > 2850
+--where sal > (select max(sal) from emp where deptno=30)
+--where sal > all (select sal from emp where deptno=30)  -- and
+where sal > any (select sal from emp where deptno=30)  -- or
+;
+
+
+-- ROWNUM
+select rownum, empno, ename, hiredate
+from emp
+where rownum<4
+order by hiredate
+;
+
+select rownum, empno, ename, hiredate from emp order by hiredate;
+
+select rownum, empno, ename, hiredate
+from (select rownum, empno, ename, hiredate from emp order by hiredate desc)  -- 인라인 뷰
+where rownum<4
+;
+
+-- 가장 급여를 많이 받는 사원 3명을 출력하자
+select rownum, ename, sal from emp order by sal desc;
+
+select rownum, ename, sal
+from (select rownum, ename, sal from emp order by sal desc)
+where rownum<6
+;
+
+--- mysql --> top     select * from emp limit 0, 3
+
+select  name from customer where custid=1;
+
+-- 스칼라 부속질의 : select 절 프리젠테이션 영역에 쓸 수 있는 부속질의, 단일행, 단일열, 단일값의 결과만 나와야한다.
+select custid, (select name from customer where customer.custid=orders.custid),
+        saleprice, orderdate
+from orders
+;
+
+-- 마당서점의 고객별  group by 
+-- 판매액을 보이시오 orders
+-- (결과는 고객이름과 고객별 판매액을 출력).
+select c.custid, c.name, sum(o.saleprice)
+from customer c, orders o
+where c.custid=o.custid(+)
+group by c.custid, c.name
+order by sum(o.saleprice)
+;
+
+select o.custid, 
+    (
+        select name from customer c where o.custid=c.custid
+    )  
+    ,  sum(o.saleprice)
+from orders o
+group by o.custid
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
