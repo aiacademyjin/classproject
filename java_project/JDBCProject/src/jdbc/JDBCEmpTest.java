31390aabc7a6897c912244d2a974328275ade8d9
aiacademyjin <aia.jin.202009@gmail.com>
2020-11-18 18:12:35 +0900

    Update JDBCEmpTest.java

--- java_project/JDBCProject/src/jdbc/JDBCEmpTest.java
+++ java_project/JDBCProject/src/jdbc/JDBCEmpTest.java
@@ -3,9 +3,7 @@ package jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 
 public class JDBCEmpTest {
