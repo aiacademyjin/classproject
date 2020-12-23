c60cf770e4fc7d8de6150c3261d8923d47b0b24a
aiacademyjin <aia.jin.202009@gmail.com>
2020-12-23 15:27:16 +0900

    Create LoginCheckFilter.java

---
+++ jsp_project/FirstWeb/src/filter/LoginCheckFilter.java
@@ -0,0 +1,51 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class LoginCheckFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		// 로그인이 여부 확인하는 Filter
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpSession session = httpRequest.getSession();
+
+		//boolean loginCheck = false;
+		
+		if(session!=null && session.getAttribute("loginInfo")!=null) {
+			chain.doFilter(request, response);
+		} else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/member/loginForm.html");
+			dispatcher.forward(request, response);
+		}
+		
+		
+			
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
