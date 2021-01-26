10129c406dab2cce7d3b8c1774ee196b4d9bda59
aiacademyjin <aia.jin.202009@gmail.com>
2021-01-26 12:47:31 +0900

    COS WEB 2021.01.26

--- spring_project/Spring-OpenProject/src/test/java/com/aia/op/DataSourceTest.java
+++ spring_project/Spring-OpenProject/src/test/java/com/aia/op/DataSourceTest.java
@@ -3,9 +3,12 @@ package com.aia.op;
 import static org.junit.Assert.fail;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +27,27 @@ public class DataSourceTest {
 	//@Setter(onMethod_ = { @Autowired })
 	@Autowired
 	private DataSource dataSource; 
+	
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
+	
+	@Test
+	public void testMybatis() {
+		
+		try(
+				SqlSession session = sqlSessionFactory.openSession(); 
+				Connection con = session.getConnection(); ){
+			
+			System.out.println(session);
+			System.out.println(con);
+			
+		} catch (Exception e) {
+			fail(e.getMessage());
+			//e.printStackTrace();
+		}
+		
+	}
 	
 	
 	@Test
