46e111852abc63360378a8a599fcfd7459f4b005
aiacademyjin <aia.jin.202009@gmail.com>
2021-01-26 12:25:57 +0900

    COS WEB 2021.01.26

---
+++ spring_project/Spring-OpenProject/src/test/java/com/aia/op/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package com.aia.op;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class HomeControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
