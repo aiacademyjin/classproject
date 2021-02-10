101208a07e3a6b2ce060bdb4189367b45096abee
aiacademyjin <aia.jin.202009@gmail.com>
2021-02-10 11:59:15 +0900

    python CSV

--- python/shop_data.py
+++ python/shop_data.py
@@ -5,6 +5,10 @@ data_header = []    # 리스트의 해더
 sample_list = []    # 리스트의 요소(행)
 insert_count = 0    # 새로운 행이 몇개 입력되었는지 카운트
 
+insert_list = []
+
+
+
 # 07_24_05_P_1.csv -> sapmle_list 리스트에 저장
 with open('07_24_05_P_1.csv') as file:
     while True:
@@ -46,7 +50,20 @@ with open('new_shop_coffe_seoul_data.csv', 'w', encoding='utf8') as new_file:
     print('쓰기 완료')
 
 
+# 현재 영업중인 서울시에 위치한 커피숍에서 스타벅스만 찾아 저장
+for shop in sample_list:
+    if shop[3].find(u'스타벅스') != -1:
+        insert_list.append(shop)
+
+with open('new_coffe_star_data.csv', 'w', encoding='utf-8') as star_file:
+    for star_shop in insert_list:
+       star_file.write(','.join(star_shop).strip('\n')+'\n') 
+    print('서울_스타벅스_리스트 파일 생성')
+
 print('파일작성이 완료되었습니다.')
+
+for s_shop in insert_list:
+    print(s_shop)
 
 
 
