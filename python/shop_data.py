bb03eed87f6c3deedfa2f3b5b82be5ed4e781e8f
aiacademyjin <aia.jin.202009@gmail.com>
2021-02-10 11:44:22 +0900

    python CSV 2021.02.10 11:44

--- python/shop_data.py
+++ python/shop_data.py
@@ -8,7 +8,7 @@ insert_count = 0    # 새로운 행이 몇개 입력되었는지 카운트
 # 07_24_05_P_1.csv -> sapmle_list 리스트에 저장
 with open('07_24_05_P_1.csv') as file:
     while True:
-        data = file.readline()
+        data = file.readline().strip('\n')
 
         # 마지막 줄 -> 더이상 가져올 데이터가 없으면 None 반환 -> 반복문을 탈출
         if not data:
@@ -19,7 +19,9 @@ with open('07_24_05_P_1.csv') as file:
             print(data_header)
         else:
             # 새로운 리스트에 추가할 행
-            insert_list = data.split(',').strip('\n')
+            insert_list = data.split(',')
+            #insert_list[6].strip()
+            
             #print(insert_list)
             # 1-> 영업중, 3-> 폐업 , 커피숍, 서울지역
             if insert_list[1] == '1' and insert_list[4] == '커피숍' and insert_list[2].find(u'서울특별시') != -1:
@@ -35,7 +37,7 @@ for i in range(20):
     print('data-',i,':', sample_list[i])
 
 with open('new_shop_coffe_seoul_data.csv', 'w', encoding='utf8') as new_file:
-    writer = csv.writer(new_file, delimiter='\t', quotechar="'", quoting=csv.QUOTE_ALL)
+    writer = csv.writer(new_file, delimiter=',', quotechar="'", quoting=csv.QUOTE_ALL)
 
     # 해더 쓰기
     writer.writerow(data_header)
